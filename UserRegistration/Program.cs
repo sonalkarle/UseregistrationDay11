69efd7084304a460ba912aac22664d5f538ba344
sonal <sonalkarle01@gmail.com>
2021-02-22 15:18:47 +0530

    Welcome'

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -6,9 +6,12 @@ namespace UserRegistration
 {
    public class Userregistrationclass
     {
+        //Pttern for  userregistration
         string firstName = "^[A-Z]{1}[a-z]{2,}$";
-    public string FirstName(string[] patternfirstname)
+        string lastName = "^[A-Z]{1}[a-z]{2,}$";
+        public string FirstName(string[] patternfirstname)
         {
+            //Validate first name
             Regex regex = new Regex(firstName);
             var answer = regex.Match(patternfirstname[1]);
             try
@@ -25,6 +28,24 @@ namespace UserRegistration
             }
 
 
+        }
+        public string LastName(string[] patternlastName)
+        {
+            //Validate the last name
+            Regex regex = new Regex(lastName);
+            var answer = regex.Match(patternlastName[1]);
+            try
+            {
+                if (!answer.Success)
+                {
+                    throw new UserregistrationcustomException(UserregistrationcustomException.ExceptionType.LAST_NAME_MESSAGE, "Last name is not valid");
+                }
+                return "Last name is valid";
+            }
+            catch
+            {
+                throw new UserregistrationcustomException(UserregistrationcustomException.ExceptionType.LAST_NAME_MESSAGE, "Last name is not valid");
+            }
         }
         static void Main(string[] args)
         {
--- Userregistrationtest/UnitTest1.cs
+++ Userregistrationtest/UnitTest1.cs
@@ -13,6 +13,9 @@ namespace Userregistrationtest
             //Arrange 
             userRegistration = new Userregistrationclass();
         }
+        /// <summary>
+        /// UC1: check wheather the first name is valid or not 
+        /// </summary>
 
         [Test]
         public void Check_Wheather_Firstname_Is_ValidOrInvalid ()
@@ -29,6 +32,25 @@ namespace Userregistrationtest
             {
                 //Assert
                 Assert.AreEqual("FirstName is not valid", exception.Message);
+            }
+        }
+        ///<summary>
+        ///UC2: check wheather the  last name is valid or not
+        ///</summary>
+        [Test]
+        public void Check_Wheather_LastName_Is_ValidOrInvalid()
+        {
+            //Act
+            string[] pattpatternlastName = { "Karle", "Sonal" };
+            string actual = " ";
+            try
+            {
+                actual = userRegistration.LastName(pattpatternlastName);
+            }
+            catch (UserregistrationcustomException exception)
+            {
+                //Assert
+                Assert.AreEqual("Last name is not valid", exception.Message);
             }
         }
     }
